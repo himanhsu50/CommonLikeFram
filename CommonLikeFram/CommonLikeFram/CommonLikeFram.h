d8c7ce2bc8ae776e9026498b3658bcc1d8562504
DEV_HIMANSHU <agarwal.himanshu@orangemantra.in>
2023-03-03 15:27:16 +0530

    initial

---
+++ CommonLikeFram/CommonLikeFram/CommonLikeFram.h
@@ -0,0 +1,18 @@
+//
+//  CommonLikeFram.h
+//  CommonLikeFram
+//
+//  Created by orange on 03/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonLikeFram.
+FOUNDATION_EXPORT double CommonLikeFramVersionNumber;
+
+//! Project version string for CommonLikeFram.
+FOUNDATION_EXPORT const unsigned char CommonLikeFramVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonLikeFram/PublicHeader.h>
+
+
---
+++ CommonLikeFram/Source/CommonLikeFram.swift
@@ -0,0 +1,12 @@
+//
+//  CommonLikeFram.swift
+//  CommonLikeFram
+//
+//  Created by orange on 03/03/23.
+//
+
+import Foundation
+
+public class CommonLikeFram{
+    static let shared = CommonLikeFram()
+}
